1448026d5bf5eec29110d6b2384ce642f2c1e610
Mina <boles.minan@gmail.com>
2026-02-16 09:38:15 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   root: './',
-  publicDir: './',
-  outDir: './dist',
+  publicDir: './public',    // خلي ملفات CSS/images هنا
+  outDir: './_site',        // ملفات build النهائية
 });
